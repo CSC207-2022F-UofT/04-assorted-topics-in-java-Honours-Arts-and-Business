7529a1d18e9e4e0d57fdbab79e978a22a9890b72
Honours-Arts-and-Business <seajun.kim@mail.utoronto.ca>
2022-10-09 23:18:12 -0400

    1st

--- src/main/java/DrivableMap.java
+++ src/main/java/DrivableMap.java
@@ -27,6 +27,14 @@ class DrivableMap {
      *       in drivable_map, then add the pair to drivable_map.
      *       Return true if the Drivable was added to drivable_map.
      */
+    boolean addDrivable(String id, Drivable object){
+        if(this.drivable_map.containsKey(id)){
+            return false;
+        }
+        this.drivable_map.put(id, object);
+        return true;
+    }
+
 
 
 
@@ -38,6 +46,15 @@ class DrivableMap {
      * iterate through drivable_map.
      */
 
+    boolean hasFasterThan(int speed){
+        for(Drivable drivable : this.drivable_map.values()){
+            if(drivable.getMaxSpeed() >= speed){
+                return true;
+            }
+        }
+        return false;
+    }
+
 
 
 
@@ -46,6 +63,16 @@ class DrivableMap {
      *       returns a List containing all of the Tradable items in
      *       drivable_map.
      */
+
+    List<Tradable> getTradable(){
+        List<Tradable> temp = new ArrayList<>();
+        for(Drivable drivable : this.drivable_map.values()){
+            if(drivable instanceof Tradable){
+                temp.add((Tradable) drivable);
+            }
+        }
+        return temp;
+    }
 
 
 
--- src/main/java/DrivableTrader.java
+++ src/main/java/DrivableTrader.java
@@ -11,3 +11,22 @@
  */
 
 import java.util.List;
+
+class DrivableTrader extends Trader<Drivable>{
+    public DrivableTrader(List<Drivable> inventory, List<Drivable> wishlist, int money) {
+        super(inventory, wishlist, money);
+    }
+
+    public DrivableTrader(int money){
+        super(money);
+    }
+
+    @Override
+    int getSellingPrice(Drivable object) {
+        if(object instanceof Tradable) {
+            return super.getSellingPrice(object) + object.getMaxSpeed();
+        }
+        return Tradable.MISSING_PRICE;
+    }
+}
+
